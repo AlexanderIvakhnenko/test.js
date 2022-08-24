6bc1dcdd9113e9347eb335533386f1ba185cbbeb
AlexanderIvakhnenko <alex.ivakhnenko1984@gmail.com>
2022-08-24 23:01:01 +0300

    сommit

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
 // Change code below this line
-function sayHi() {
-  console.log("Hello, this is my first function!")
-};
-sayHi("Hello, this is my first function!");
+// function sayHi() {
+//   console.log("Hello, this is my first function!")
+// };
+// sayHi("Hello, this is my first function!");
+
+// const message = "JavaScript is awesome!";
+// alert(message);
+// console.log(message);
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const value = prompt("Please enter a number!");
+// console.log(typeof value); // "string"
+// console.log(value); // "5"
+
+// const guestName = "Манго";
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting); // "Welcome Mango, your room number is 207!"
+
+
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.length); // 19
+// console.log("There is nothing impossible to him who will try".length); // 47
+
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.toLowerCase()); // "welcome to bahamas!"
+// console.log(message.toUpperCase()); // "WELCOME TO BAHAMAS!"
+// console.log(message); // "Welcome to Bahamas!"
+
+
+
+// const BRAND_NAME = "SAMSUNG";
+// const userInput = "saMsUng";
+// const normalizedToUpperCaseInput = userInput.toUpperCase();
+
+// console.log(userInput); // 'saMsUng'
+// console.log(userInput === BRAND_NAME); // false
+// console.log(normalizedToUpperCaseInput); // 'SAMSUNG'
+// console.log(normalizedToUpperCaseInput === BRAND_NAME); // true
+
+
+
+
+// const jsFileName = "script.js";
+// const minifiedJsFileName = jsFileName.replace(".js", ".min.js");
+// console.log(minifiedJsFileName); // "script.min.js"
+
+// const cssFileNames = "styles.css, about.css, portfolio.css";
+// const minifiedCssFileNames = cssFileNames.replaceAll(".css", ".min.css");
+// console.log(minifiedCssFileNames); // "styles.min.css, about.min.css, portfolio.min.css"
+
+
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// // Для чисел меньше чем порог срабатывает continue, выполнение тела прекращается
+// // и управление передаётся на следующую итерацию.
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+
+//   console.log(`Число больше чем ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
+
+
+
+
+const a = ["Mango"];
+// Так как a это массив, в b записывается ссылка на уже существующий
+// массив в памяти. Теперь a и b указывают на один и тот же массив.
+const b = a;
+console.log(a); // ["Mango"]
+console.log(b); // ["Mango"]
+
+// Изменим массив, добавив еще один элемент, используя указатель из a
+a.push("Poly");
+console.log(a); // ["Mango", "Poly"]
+
+// b также изменилось, потому что b, как и a,
+// просто содержит ссылку на одно и то же место в памяти
+console.log(b); // ["Mango", "Poly"]
+
+// Результат повторяется
+b.push("Ajax");
+console.log(a); // ["Mango", "Poly", "Ajax"]
+console.log(b); // ["Mango", "Poly", "Ajax"]
